5d564b863d04c42e9aa07641d461b6ed03bafbfc
David <davidleonasd@gmail.com>
2020-09-17 03:02:52 +0700

    add test connection to database before starting

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app=express();
 const port=process.env.PORT || 3000;
-
+const {testConnection} = require('./db/connection');
 const c_Dompet = require('./controller/dompet');
 const c_Kategori = require('./controller/kategori');
 const c_Transaksi = require('./controller/transaksi');
@@ -13,5 +13,12 @@ app.use('/kategori', c_Kategori);
 app.use('/transaksi', c_Transaksi);
 app.use('/laporan', c_Laporan);
 
-app.listen(port)
-console.log("Atom-Pocket listening on port : "+port);
+testConnection((err, res)=>{
+    if(err){
+        console.error(`Atom-Pocket testing connection to database failed`);
+        console.error(`Atom-Pocket terminated`);
+        return;
+    }
+    app.listen(port)
+    console.log("Atom-Pocket listening on port : "+port);
+})
