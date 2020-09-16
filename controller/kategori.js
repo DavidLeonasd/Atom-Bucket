2bf6c73dc82c1cc45bbb9f6dc3eca34664e83ce3
David <davidleonasd@gmail.com>
2020-09-16 15:56:15 +0700

    finish kategori module

--- controller/kategori.js
+++ controller/kategori.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const router=express.Router();
+const m_kategori=require('../model/m_kategori');
+const utils=require('../utils');
 
-
-router.get('/create', function (req, res) {
-    res.send(req.params);
+router.get('/create', function (req, resp) {
+    if(!req.query.nama) {
+        utils.expressSendErrorResponse(resp, utils.HTTP_RESPONSE_CODE_422_UNPROCESSABLE_ENTITY, 'nama is mandatory');
+        return;
+    }
+    var nama=req.query.nama || '', deskripsi=req.query.deskripsi || '';
+    m_kategori.create(nama, deskripsi,(err, res)=>{
+        if(err)utils.expressSendErrorResponse(resp, utils.HTTP_RESPONSE_CODE_500_INTERNAL_SERVER_ERROR, 'failed to execute query to database')
+        else utils.expressSendResponse(resp, utils.HTTP_RESPONSE_CODE_200_OK, 'success');
+    })
 })
 
-router.get('/update', function (req, res) {
-    res.send(req.params);
+router.get('/update', function (req, resp) {
+    if(!req.query.id || !req.query.nama){
+        utils.expressSendErrorResponse(resp, utils.HTTP_RESPONSE_CODE_422_UNPROCESSABLE_ENTITY, 'id and nama is mandatory');
+        return;
+    }
+    var id=req.query.id, nama=req.query.nama || '', deskripsi=req.query.deskripsi || '';
+    m_kategori.update(id, nama, deskripsi,(err, res)=>{
+        if(err)utils.expressSendErrorResponse(resp, utils.HTTP_RESPONSE_CODE_500_INTERNAL_SERVER_ERROR, 'failed to execute query to database')
+        else utils.expressSendResponse(resp, utils.HTTP_RESPONSE_CODE_200_OK, 'success');
+    })
 })
 
-router.get('/get', function (req, res) {
-    res.send(req.params);
+router.get('/get', function (req, resp) {
+    var id=req.query.id;
+    m_kategori.read(id,(err, res)=>{
+        if(err)utils.expressSendErrorResponse(resp, utils.HTTP_RESPONSE_CODE_500_INTERNAL_SERVER_ERROR, 'failed to execute query to database')
+        else if(res.rows.length<=0)utils.expressSendErrorResponse(resp, utils.HTTP_RESPONSE_CODE_404_NOT_FOUND, 'No Data Found', );
+        else utils.expressSendResponseAndData(resp, utils.HTTP_RESPONSE_CODE_200_OK, 'success', res.rows);
+    })
 })
-
 
 
 
---
+++ model/m_kategori.js
@@ -0,0 +1,33 @@
+const {getClient} = require('../db/connection');
+const table_name='kategori';
+const table_columns=['nama','deskripsi'];
+module.exports.create=function(nama, deskripsi, callback){
+    var query=`INSERT INTO ${table_name}(nama, deskripsi) VALUES('${nama}', '${deskripsi}')`;
+    getClient((err, client, release)=>{
+        client.query(query, (err, res) => {
+            callback(err, res);
+          })
+        release();
+    });
+}
+
+module.exports.update=function(id, nama, deskripsi, callback){
+    var query=`UPDATE ${table_name} SET nama='${nama}', deskripsi='${deskripsi}' WHERE id=${id}`;
+    getClient((err, client, release)=>{
+        client.query(query, (err, res) => {
+            callback(err, res);
+          })
+        release();
+    });
+}
+
+module.exports.read=function(id, callback){
+    var query=`SELECT * FROM ${table_name}`;
+    if(id)query+=` WHERE id=${id}`;
+    getClient((err, client, release)=>{
+        client.query(query, (err, res) => {
+            callback(err, res);
+          })
+        release();
+    });
+}
